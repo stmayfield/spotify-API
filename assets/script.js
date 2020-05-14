f891f7d30720018841613fe4a530e1ea4b9ad4b2
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-05-13 20:47:30 -0700

    1st revert to 'jquery empty func'

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-
 let discoverBtn = document.getElementById("discover");
 let apikey = "368598-musicbuf-RZ4G3NI6";
 let addBtn = document.getElementById("add")
@@ -60,17 +59,23 @@ renderTrackList(testTrackList);
 
 getArtist();
 
+var authButton = $("#widget").append($("<button>").html("Allow Access"));
+authButton.click(function () {
+    window.location.href = authEndpoint + clientID + "&redirect_uri=" + redirectURI + '&response_type=token';
+    buildAuthLink()
+})
+
 
 var authEndpoint = 'https://accounts.spotify.com/authorize?client_id=';
 var clientID = "87da17f3514b4a86854820f3d7804bb0"
 var redirectURI = "https:%2F%2Fstmayfield.github.io%2Fspotify-API%2F"
 var artist = /*$("#newItem").val()*/ "taylor swift";
+let accessToken
+var URI = "0Hoc8rluBkPaXu9pQAq1x6"
+var authToken = "BQCNHtSVriShcFxlJpG-stAvmPbF-aMmLKkoyq9F0EqeIVHHiBgElaxU5Tvb2WmgqBQfxlgXUbDfjrHN-OsiUXCiysFRL1dCMDckpJqaJxcdcovSu_ifSBiE0DsJxfz5YyvU1_Rr3skHgFjZV07vnAa0WcIF3Ss"
+var queryURL = "https://api.spotify.com/v1/search/q=" + artist + "&type=artist"
 
 
-var authButton = $("#widget").append($("<button>").html("Allow Access"));
-authButton.click(function () {
-    window.location.href = authEndpoint + clientID + "&redirect_uri=" + redirectURI + '&response_type=token';
-})
 
 /*
 'https://accounts.spotify.com/authorize?client_id=87da17f3514b4a86854820f3d7804bb0&redirect_uri=https://stmayfield.github.io/spotify-API/&response_type=token'
@@ -78,83 +83,48 @@ authButton.click(function () {
 */
 
 
+function buildAuthLink() {
+    const hash = window.location.hash
+        .substring(1)
+        .split('&')
+        .reduce(function (initial, item) {
+            if (item) {
+                var parts = item.split('=');
+                initial[parts[0]] = decodeURIComponent(parts[1]);
+            }
+            return initial;
+        }, {});
+    window.location.hash = '';
 
+    let _token = hash.access_token;
+    const authEndpoint = 'https://accounts.spotify.com/authorize';
+    const clientID = "87da17f3514b4a86854820f3d7804bb0"
+    const redirectURI = "https://stmayfield.github.io/spotify-API/"
+    const scopes = [
+        'user-read-private',
+        'user-read-email'
+    ];
 
+    if (!_token) {
+        window.location = `${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectURI}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
+    }
 
-// Event Trigger for Spotify Auth
-
-
-
-
-
-
-// Spotify Authorization Token 
-let accessToken
-var queryURL = "https://api.spotify.com/v1/search/q=" + artist + "&type=artist"
-
-var artist = $("#newItem").val();
-const hash = window.location.hash
-    .substring(1)
-    .split('&')
-    .reduce(function (initial, item) {
-        if (item) {
-            var parts = item.split('=');
-            initial[parts[0]] = decodeURIComponent(parts[1]);
+    // Spotify API
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        Accept: "application/json",
+        beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', 'Bearer ' + _token); },
+        success: function (response) {
+            console.log(response)
+            iFrameW();
         }
-        return initial;
-    }, {});
-window.location.hash = '';
+    })
 
-let _token = hash.access_token;
-
-const scopes = [
-    'user-read-private',
-    'user-read-email'
-];
-
-if (!_token) {
-    window.location = `${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectURI}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
 }
 
 
 
-// Event Trigger for Spotify Auth
-
-// var authButton = $("#widget").append($("<button>").html("Allow Access"));
-// authButton.click(function () {
-//     buildAuthLink();
-// })
-
-
-// Spotify API
-$.ajax({
-    url: queryURL,
-    method: "GET",
-    Accept: "application/json",
-    Authorization: "Bearer " + authToken,
-    beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', 'Bearer ' + _token); },
-    success: function (response) {
-        console.log(response)
-        var artistID = response.artists.items[0].id
-        var queryURL2 = "https://api.spotify.com/v1/artists/" + artistID + "/top-tracks"
-        function ajax2() {
-            $.ajax({
-                url: queryURL2,
-                method: "GET",
-                Accept: "application/json",
-                success: function (response) {
-                    console.log(response)
-                    iFrameW();
-                }
-            })
-        }
-        ajax2()
-    }
-})
-
-// var URI = "6mswcNfl5UULnG5fvg5Fty?si=i2tgZLkeRQeZSAInNvm_ew"
-
-// Spotify Widget
 function iFrameW() {
     $("#widget").empty();
     var iFrameW = $("<iframe>").attr({
