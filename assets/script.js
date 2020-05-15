345552575e6111e5093de2388e0c4b249c54b067
Stephen Mayfield <61091353+stmayfield@users.noreply.github.com>
2020-05-15 01:03:23 -0700

    hrngh

--- assets/script.js
+++ assets/script.js
@@ -66,7 +66,7 @@ function iFrameW(URI) {
     $("#widget").empty();
     var iFrameW = $("<iframe>").attr({
         src: "https://open.spotify.com/embed/track/" + URI,
-        width: "150",
+        width: "250",
         height: "80",
         frameborder: "0",
         allowtransparency: "true",
@@ -76,35 +76,35 @@ function iFrameW(URI) {
 };
 
 
-// Set Auth button to Discover button
-var authButton = $("#widget").append($("<button>").html("Allow Access"));
-authButton.click(function () {
-    const hash = window.location.hash
-        .substring(1)
-        .split('&')
-        .reduce(function (initial, item) {
-            if (item) {
-                var parts = item.split('=');
-                initial[parts[0]] = decodeURIComponent(parts[1])
-            }
-            return initial;
-        }, {});
-    window.location.hash = '';
+function authenticateLoad() { }
+const hash = window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce(function (initial, item) {
+        if (item) {
+            var parts = item.split('=');
+            initial[parts[0]] = decodeURIComponent(parts[1])
+        }
+        return initial;
+    }, {});
+window.location.hash = '';
 
-    let _token = hash.access_token;
-    var authEndpoint = "https://accounts.spotify.com/authorize";
-    var clientID = "87da17f3514b4a86854820f3d7804bb0";
-    // Set new URI to Live Site
-    var redirectURI = "https://stmayfield.github.io/spotify-API/";
-    var scope = [
-        "user-top-read"
-    ];
+let _token = hash.access_token;
+var authEndpoint = "https://accounts.spotify.com/authorize";
+var clientID = "87da17f3514b4a86854820f3d7804bb0";
+// Set new URI to Live Site
+var redirectURI = "https://stmayfield.github.io/spotify-API/";
+var scope = [
+    "user-top-read"
+];
 
-    if (!_token) {
-        window.location = authEndpoint + "?client_id=" + clientID + "&redirect_uri=" + redirectURI + "&scope=" + scope.join("%20") + "&response_type=token&show_dialog=true";
-    }
+if (!_token) {
+    window.location = authEndpoint + "?client_id=" + clientID + "&redirect_uri=" + redirectURI + "&scope=" + scope.join("%20") + "&response_type=token&show_dialog=true";
+}
 
 
+function searchAPI() {
+    // Store recent search in localStorage
     var artistResult = $("#newItem").val().trim();
     var queryURL = "https://api.spotify.com/v1/search?q=" + artistResult + "&type=artist";
 
@@ -127,6 +127,24 @@ authButton.click(function () {
         });
     });
 
-});
+};
+
+
+
+
+
+
+
+
+
+// Set Auth button to Discover button
+var authButton = $("#widget").append($("<button>").html("Allow Access"));
+authButton.click(
+    authenticateLoad()
+);
+authButton.click(
+    searchAPI()
+);
+
 
 
